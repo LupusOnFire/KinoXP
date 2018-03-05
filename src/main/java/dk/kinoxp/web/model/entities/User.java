89640d2b705867239e37b01ec8bddfbc2f8efdc7
Mehmet Deniz <mehm0639@stud.kea.dk>
2018-03-05 10:52:44 +0100

    Created User entity

---
+++ src/main/java/dk/kinoxp/web/model/entities/User.java
@@ -0,0 +1,42 @@
+package dk.kinoxp.web.model.entities;
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(int userId, String username, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
