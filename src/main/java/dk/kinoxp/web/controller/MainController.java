9eaa747f52ee67a80cf0c98df2666f9183d78bb9
LupusOnFire <bertram@lupusmedia.dk>
2018-03-08 13:59:18 +0100

    updated create-cinema to use RequestParameter

--- src/main/java/dk/kinoxp/web/controller/MainController.java
+++ src/main/java/dk/kinoxp/web/controller/MainController.java
@@ -1,5 +1,6 @@
 package dk.kinoxp.web.controller;
 
+import dk.kinoxp.web.model.entities.Cinema;
 import dk.kinoxp.web.model.repositories.CinemaRepository;
 import dk.kinoxp.web.model.repositories.SeatRepository;
 import dk.kinoxp.web.model.services.CinemaCreator;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,10 +28,17 @@ public class MainController {
     public String index(HttpServletRequest request){
         return "index";
     }
-    @RequestMapping(value = {"create-cinema"}, method = RequestMethod.GET)
-    public String createCinema(Model model) {
-        CinemaCreator cinemaCreator = new CinemaCreator(seatRepository, cinemaRepository);
-        cinemaCreator.createCinemaAndSeats(2, 25, 16);
+    @RequestMapping(value = {"create-cinema"}, method = RequestMethod.GET, params = {"cinemaId", "cinemaHeight", "cinemaWidth", "rowCount", "columnCount"})
+    public String createCinema(Model model, @RequestParam int cinemaId, @RequestParam double cinemaHeight, @RequestParam double cinemaWidth, @RequestParam int rowCount, @RequestParam int columnCount) {
+        /* Example usage :
+           http://127.0.0.1:8080/create-cinema?cinemaId=3&cinemaWidth=0&cinemaHeight=0&rowCount=20&columnCount=10
+         */
+        Cinema cinema = cinemaRepository.findById(cinemaId);
+        if (cinema == null) {
+            cinema = cinemaRepository.save(new Cinema(cinemaId, cinemaHeight, cinemaWidth));
+        }
+        CinemaCreator cinemaCreator = new CinemaCreator();
+        seatRepository.saveAll(cinemaCreator.createSeats(cinema, rowCount, columnCount));
         return "index";
     }
 }
