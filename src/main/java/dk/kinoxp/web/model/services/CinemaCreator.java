36ec93d3040643110ccafc8686d4e9b32ee45a3f
LupusOnFire <bertram@lupusmedia.dk>
2018-03-08 13:51:19 +0100

    Updated CinemaCreator to return List of Seats

--- src/main/java/dk/kinoxp/web/model/services/CinemaCreator.java
+++ src/main/java/dk/kinoxp/web/model/services/CinemaCreator.java
@@ -10,25 +10,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CinemaCreator {
-    private CinemaRepository _cinemaRepository;
-    private SeatRepository _seatRepository;
-
-    public CinemaCreator(SeatRepository seatRepository, CinemaRepository cinemaRepository) {
-        _seatRepository = seatRepository;
-        _cinemaRepository = cinemaRepository;
+    public CinemaCreator() {
     }
 
 
-    public void createCinemaAndSeats(int cinemaId, int rows, int columnCount) {
+    public List<Seat> createSeats(Cinema cinema, int rows, int columnCount) {
         List<Seat> seats = new LinkedList<>();
-        Cinema cinema = _cinemaRepository.findById(cinemaId);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columnCount; j++) {
                 Seat seat = new Seat(i+1, j+1, cinema);
                 seats.add(seat);
             }
         }
-        _cinemaRepository.save(cinema);
-        _seatRepository.saveAll(seats);
+        return seats;
     }
 }
